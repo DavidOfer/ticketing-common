ba729bf40fe646d80f27b2745981ec412c454f56
DavidOfer <oferd90@gmail.com>
2022-06-07 15:03:11 +0300

    Updates

--- src/events/subjects.ts
+++ src/events/subjects.ts
@@ -2,7 +2,7 @@ export enum Subjects {
     TicketCreated = 'ticket:created',
     TicketUpdated = 'ticket:updated',
     OrderCreated = 'order:created',
-    OrderCancalled = 'order:cancelled',
+    OrderCancelled = 'order:cancelled',
     ExpirationComplete ='expiration:complete'
 }
 
