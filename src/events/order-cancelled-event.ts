1873e50511738dac0fd3b1ae1cb20999a55a2a21
DavidOfer <oferd90@gmail.com>
2022-06-07 15:05:48 +0300

    Updates

--- src/events/order-cancelled-event.ts
+++ src/events/order-cancelled-event.ts
@@ -2,7 +2,7 @@ import { Subjects } from "./subjects";
 import { OrderStatus } from "./types/order-status";
 
 export interface OrderCancelledEvent {
-  subject: Subjects.OrderCancalled;
+  subject: Subjects.OrderCancelled;
   data: {
     id: string;
     version: number;
